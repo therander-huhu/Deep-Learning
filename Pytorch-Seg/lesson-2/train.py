44823d10e84073ccfcf11e0050b4cca94e6b200d
JimHan <876674603@163.com>
2022-02-27 19:01:58 +0800

    修改训练日志

--- Pytorch-Seg/lesson-2/train.py
+++ Pytorch-Seg/lesson-2/train.py
@@ -18,6 +18,7 @@ def train_net(net, device, data_path, epochs=40, batch_size=1, lr=0.00001):
     best_loss = float('inf')
     # 训练epochs次
     for epoch in range(epochs):
+        print('epoch/train', epoch)
         # 训练模式
         net.train()
         # 按照batch_size开始训练
@@ -26,12 +27,12 @@ def train_net(net, device, data_path, epochs=40, batch_size=1, lr=0.00001):
             # 将数据拷贝到device中
             image = image.to(device=device, dtype=torch.float32)
             label = label.to(device=device, dtype=torch.float32)
-            print(image.shape)
+            # print(image.shape)
             # 使用网络参数，输出预测结果
             pred = net(image)
             # 计算loss
             loss = criterion(pred, label)
-            # print('Loss/train', loss.item())
+            print('Loss/train', loss.item())
             # 保存loss值最小的网络参数
             if loss < best_loss:
                 best_loss = loss
